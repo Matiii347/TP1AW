d3a8bc5d949c9f77ee29fbd15284a20ebf1ff086
Matiii347 <matiasracingente@gmail.com>
2025-10-16 10:26:04 -0300

    Cambios para ver el front de las cards

--- CRUD/modules/productos/controlador.productos.mjs
+++ CRUD/modules/productos/controlador.productos.mjs
@@ -1,6 +1,8 @@
 import * as modelo from '../productos/modelo.productos.mjs'
 
-export async function ObtenerProductos(req,res) {
-    const datos= await modelo.ObtenerProductos()
-    res.json(datos)
+export async function ObtenerProductos(req, res) {
+    // Leemos el parámetro 'categoria' de la URL
+    const { categoria } = req.query;
+    const datos = await modelo.ObtenerProductos(categoria); // Se lo pasamos al modelo
+    res.json(datos);
 }
--- CRUD/modules/productos/modelo.productos.mjs
+++ CRUD/modules/productos/modelo.productos.mjs
@@ -1,7 +1,15 @@
 import pool from '../../config/conexion-BD.mjs'
 
-export async function ObtenerProductos() {
-    const resultado = await pool.query('Select * from autos')
-    const productos = resultado.rows
-    return productos
+export async function ObtenerProductos(categoria) {
+    let consulta = 'SELECT * FROM autos';
+    const valores = [];
+
+    if (categoria) {
+        consulta += ' WHERE categoria = $1'; // Agregamos el filtro
+        valores.push(categoria);
+    }
+
+    const resultado = await pool.query(consulta, valores);
+    const productos = resultado.rows;
+    return productos;
 }
